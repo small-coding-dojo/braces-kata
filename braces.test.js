4d526a553a33d5c93d65953cb90def5946940eda
Niklas Rueber <nrueber@igus.net>
2022-06-30 17:44:37 +0200

    feat: add recognize missmatched parantheses. It will not work in some borderline cases. Only for different delimiters, probably not for same delimiters. NOT EFFICIENT AT ALL! - Might not matter Will be fixed in prod. Temp solution.
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Holger Janssen-Kroll <holger.janssen-kroll@aleri.de>
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>
    Co-authored-by: Tugbay Atilla <tugbayatilla@gmail.com>
    Co-authored-by: Falk Kühnel <falk@kuehnel.eu>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>

--- braces.test.js
+++ braces.test.js
@@ -1,8 +1,42 @@
-function is_balanced(s, s2) {
-    return true;
+function is_balanced(inputString, delimiterPair) {
+    for(let i = 0 ; i < delimiterPair.length; i+=2) {
+        opening = delimiterPair[i]
+        closing = delimiterPair[i+1]
+        stack = [];
+
+        for (characterOfInput of inputString) {
+            if (characterOfInput == opening) {
+                stack.push(opening)
+            } else if (characterOfInput == closing) {
+                if (typeof (stack.pop()) == "undefined") {
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length == 0;
 }
 
-test("test", () => {
+test("input contains simple pair of matching opening and closing parentheses => true", () => {
     expect(is_balanced("(Sensei says yes!)", "()")).toBe(true);
 });
 
+test("input does not contain matching opening and closing parentheses => false", () => {
+    expect(is_balanced("(Sensei says no!", "()")).toBe(false);
+});
+
+test("input starts with closing and ends with opening parenthesis => false", () => {
+    expect(is_balanced(")Sensei says no!(", "()")).toBe(false);
+});
+
+test("should reject a single closing bracket", () => {
+    expect(is_balanced(")", "()")).toBe(false);
+});
+
+test("two valid inputs return true", () => {
+    expect(is_balanced("(this is good{!})", "(){}")).toBe(true);
+});
+
+test("this is bad", () => {
+    expect(is_balanced("(this is bad })", "(){}")).toBe(false);
+});
